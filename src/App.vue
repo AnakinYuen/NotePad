daf36825728465f9bca68e15728ced33a4024471
Anakin Yuen <anakinyuen1024@gmail.com>
2019-04-04 00:23:08 +0800

    :hatching_chick: initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+@import "~normalize.css";
+
+* {
+  box-sizing: border-box;
+}
+
+body {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
