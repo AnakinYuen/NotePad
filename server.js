54d5472868e5a6c131e353a8153f792440bc9262
Anakin Yuen <anakinyuen1024@gmail.com>
2019-04-05 00:12:05 +0800

    :star: feature: enable express to serve static gzip file

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const expressStaticGzip = require("express-static-gzip");
 const path = require("path");
 
 const app = express();
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(
+  expressStaticGzip(path.join(__dirname, "dist"), {
+    enableBrotli: true
+  })
+);
 
 const port = process.env.PORT || 80;
 app.listen(port);
